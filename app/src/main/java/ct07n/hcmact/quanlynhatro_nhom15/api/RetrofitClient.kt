79e662a05e297d0cd1e2149a2c80584fcc6b79f6
Vo-Hoa-Thuan <vohoathuan2828@gmail.com>
2024-05-27 17:04:42 +0700

    thuan update

--- app/src/main/java/ct07n/hcmact/quanlynhatro_nhom15/api/RetrofitClient.kt
+++ app/src/main/java/ct07n/hcmact/quanlynhatro_nhom15/api/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.50.97:3000/"
+    private const val BASE_URL = "http://192.168.1.44:3000/"
 
     val instance: Retrofit by lazy {
         Retrofit.Builder()
